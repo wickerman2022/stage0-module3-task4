ad2e7a83016f95cafa1edb23696c9da92fd7e965
wickerman2019 <sverige@i.ua>
2023-01-07 10:46:07 +0200

    Fix compilation error

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a, b, c;
-        c = (9 * a * a - 5 * b + 2 - 7) * ((a + b - 4 * a * b) / 2);
+        int a = 0, b = 0;
+        int c = (9 * a * a - 5 * b + 2 - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
