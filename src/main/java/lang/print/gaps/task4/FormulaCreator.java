8ca0ad9966204e7c76a46f826a9675ffd5fd572a
wickerman2019 <sverige@i.ua>
2023-01-07 10:43:14 +0200

    Setting types and formula

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a, b, c;
+        c = (9 * a * a - 5 * b + 2 - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+        System.out.print(c);
+        System.out.print(h);
+        System.out.print(a);
+        System.out.println(r);
     }
 }
